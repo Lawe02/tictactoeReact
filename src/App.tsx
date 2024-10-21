321327974dd780ab0576336e7293d10b0be51023
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-10-21 10:54:47 +0200

    wip

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function Square() {
+  return (<button className="square">1</button>)
+}
+
+export default function Board() {
+
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+   );
+ }
+
