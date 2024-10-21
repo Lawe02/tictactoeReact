43e5d42b0da73196078f6b1a6f944084a2c53edf
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-10-21 11:17:57 +0200

    feat:add crosses when button is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import './App.css'
+import { useState } from 'react'
 
-function Square({ value }) {
-  return (<button className="square">{value}</button>)
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
-
 export default function Board() {
 
   return (
     <>
-      <div className="board-row">
+     <div className="board-row">
         <Square />
         <Square />
         <Square />
